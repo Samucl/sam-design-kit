40fb435aaafe574de56ea3b5e5c1480178ec926d
Samuel Laisaar <slaisaar00@gmail.com>
2024-03-24 23:30:48 +0200

    Improving Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,15 +7,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sam-design-kit.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Samucl', // Usually your GitHub org/user name.
+  projectName: 'sam-design-kit', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
